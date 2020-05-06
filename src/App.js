ff2594049743556d09a9e29de439f6bcddb675ed
Puria Nafisi Azizi <puria@dyne.org>
2020-05-06 10:13:00 +0200

    Add screenshot

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const Zen = require('@lorena-ssi/zenroom-lib')
+
+const keygen = async () => {
+	let z = new Zen()
+	let alice_keypair = await z.newKeyPair('Alice')
+	console.log(alice_keypair)
+}
 
 function App() {
+  keygen()
   return (
     <div className="App">
       <header className="App-header">
